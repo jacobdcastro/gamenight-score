9587f92f96f66f63f3e31b03f119e0cc9ca65776
Jacob D. Castro <jdcastro.business@gmail.com>
2021-04-29 11:12:55 -0700

    export functions from index files in directories

---
+++ models/index.ts
@@ -0,0 +1,3 @@
+export { default as Player } from './Player';
+export { default as Game } from './Game';
+export { default as Round } from './Round';
---
+++ routes/index.ts
@@ -0,0 +1,1 @@
+export { default as playerRouter } from './api/player';
